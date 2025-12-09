2e359693318459413ded0347184da88774889343
surajlearninig-bit <surajlearninig@gmail.com>
2025-12-09 17:25:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Starting Server 1..."
-node /app/my-test-site/server.js &
+cd /app/my-test-site/ && node server.js &
 
 echo "Starting Server 2..."
-node /app/test_server_2/server2.js &
+cd /app/test_server_2/ && node server2.js &
 
 echo "Starting Health Monitor..."
 python3 /app/server_health.py
